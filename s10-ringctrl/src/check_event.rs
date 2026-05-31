d13bfcf182c43c3c7cd1298394d29f4224e63c96
joey <stranded@limbo.md>
2026-05-31 15:42:13 +0800

    chore: remove temporary check_event.rs

--- s10-ringctrl/src/check_event.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn check_abs_mt_tracking_id() {
-        use evdev::AbsoluteAxisType;
-        println!("ABS_MT_TRACKING_ID code = {}", AbsoluteAxisType::ABS_MT_TRACKING_ID.0);
-    }
-}
